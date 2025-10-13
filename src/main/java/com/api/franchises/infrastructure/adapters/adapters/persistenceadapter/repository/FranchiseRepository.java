d25e2d38aefc15bfc19f525d6bd5f0f7e9c96d27
John Angel <jairoanngelll@gmail.com>
2025-10-12 21:59:22 -0500

    feat(infra): define mapper and repository for franchise

---
+++ src/main/java/com/api/franchises/infrastructure/adapters/adapters/persistenceadapter/repository/FranchiseRepository.java
@@ -0,0 +1,11 @@
+package com.api.franchises.infrastructure.adapters.adapters.persistenceadapter.repository;
+
+import com.api.franchises.infrastructure.adapters.adapters.persistenceadapter.entity.FranchiseEntity;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface FranchiseRepository extends ReactiveMongoRepository<FranchiseEntity, String> {
+
+}
