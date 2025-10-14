516ef7e7e601f891edc1c6b0cfac6f87d9c18265
John Angel <jairoanngelll@gmail.com>
2025-10-14 09:54:05 -0500

    fix: validate exist branch and rowAffected

--- src/main/java/com/api/franchises/domain/usecase/BranchUseCase.java
+++ src/main/java/com/api/franchises/domain/usecase/BranchUseCase.java
@@ -1,6 +1,7 @@
 package com.api.franchises.domain.usecase;
 
 import com.api.franchises.domain.api.BranchServicePort;
+import com.api.franchises.domain.constants.Constants;
 import com.api.franchises.domain.enums.TechnicalMessage;
 import com.api.franchises.domain.exceptions.BusinessException;
 import com.api.franchises.domain.model.Branch;
@@ -36,7 +37,7 @@ public class BranchUseCase implements BranchServicePort {
                 .flatMap(exists -> exists
                 ? branchPersistencePort.updateNameBranch(franchiseId, branchId, newName)
                 : Mono.error(new BusinessException(TechnicalMessage.FRANCHISE_NOT_FOUND)))
-                .flatMap(rows -> rows > 0
+                .flatMap(rows -> rows > Constants.MIN_VALUE
                 ? Mono.<Void>empty()
                 : Mono.error(new BusinessException(TechnicalMessage.BRANCH_NOT_IN_FRANCHISE_OR_NOT_FOUND)));
     }
--- src/main/java/com/api/franchises/domain/usecase/FranchiseUseCase.java
+++ src/main/java/com/api/franchises/domain/usecase/FranchiseUseCase.java
@@ -1,6 +1,7 @@
 package com.api.franchises.domain.usecase;
 
 import com.api.franchises.domain.api.FranchiseServicePort;
+import com.api.franchises.domain.constants.Constants;
 import com.api.franchises.domain.enums.TechnicalMessage;
 import com.api.franchises.domain.exceptions.BusinessException;
 import com.api.franchises.domain.model.Franchise;
@@ -26,7 +27,7 @@ public class FranchiseUseCase implements FranchiseServicePort {
     @Override
     public Mono<Void> updateNameFranchise(Long franchiseId, String newName) {
         return franchisePersistencePort.updateNameFranchise(franchiseId, newName)
-                .flatMap(rows -> rows > 0
+                .flatMap(rows -> rows > Constants.MIN_VALUE
                 ? Mono.<Void>empty()
                 : Mono.error(new BusinessException(TechnicalMessage.FRANCHISE_NOT_FOUND)));
     }
